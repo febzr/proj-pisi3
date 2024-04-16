8a6e51888e5e3ab957d9d75766613e93f9eaf271
Felipe Bezerra <136595371+febzr@users.noreply.github.com>
2024-04-16 15:48:14 -0300

    Alteração para resolver duplicações

--- gerar_html.py
+++ gerar_html.py
@@ -1,7 +1,7 @@
-import numpy as np
 import pandas as pd
 from ydata_profiling import ProfileReport
 
-df = pd.read_csv('heart_2022_no_nans.csv')
+df = pd.read_csv('proj-pisi3\heart_2022_no_nans.csv')
+s = df.duplicated(keep=	'first')
 perfil = ProfileReport(df, title='Dados obtidos da base: Indicators of Heart Disease 2022 (no NaNs)')
 perfil.to_file('perfil_dados_base.html')
