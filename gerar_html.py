626d3e5e25636174c280f155ed872e51a87d408a
Felipe Bezerra <136595371+febzr@users.noreply.github.com>
2024-04-16 20:50:56 -0300

    Correção da base de dados
    Correção da base de dados e código relacionado

--- gerar_html.py
+++ gerar_html.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from ydata_profiling import ProfileReport
 
-df = pd.read_parquet('heart_2022_no_nans.parquet')
-perfil = ProfileReport(df, title='Dados obtidos da base: Indicators of Heart Disease 2022 (no NaNs)')
+df = pd.read_parquet('heart_2020_cleaned.parquet')
+perfil = ProfileReport(df, title='Dados obtidos da base: Indicators of Heart Disease 2020 (cleaned)')
 perfil.to_file('perfil_dados_base.html')
