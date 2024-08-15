0ffab91751200489ab1188aa213d64894dfafbd0
Yago-Ferraz <yagomouraferraz100@gmail.com>
2024-08-14 22:14:40 -0300

    retirada da venv

--- csv_to_parquet.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('heart_2020_cleaned.csv')
-df.to_parquet('heart_2020_cleaned.parquet')
--- gerar_html.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from ydata_profiling import ProfileReport
-
-df = pd.read_parquet('heart_2020_cleaned.parquet')
-perfil = ProfileReport(df, title='Dados obtidos da base: Indicators of Heart Disease 2020 (cleaned)')
-perfil.to_file('perfil_dados_base.html')
