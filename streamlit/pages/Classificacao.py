e958e212727b0babab085fa8a24b8fc0bff6096c
Felipe Bezerra <136595371+febzr@users.noreply.github.com>
2024-09-04 00:16:15 -0300

    Streamlit - Hotfix

--- streamlit/pages/Classificacao.py
+++ streamlit/pages/Classificacao.py
@@ -29,51 +29,29 @@ st.set_page_config(page_title='Classificação', page_icon=':heart:', layout='ce
 
 st.title("CLASSIFICAÇÃO")
 
-dist6 = st.expander('Grid Search')
-
-with dist6:
-    gtreino.update_layout(width=670)
-    st.plotly_chart(gtreino)
-    gteste.update_layout(width=670)
-    st.plotly_chart(gteste)
+gtreino.update_layout(width=670)
+st.plotly_chart(gtreino)
+gteste.update_layout(width=670)
+st.plotly_chart(gteste)
 
 #RDForest
+st.subheader('Feature Importances')
 dist1 = st.expander('Random Forest')
 
 with dist1:
-    rdfor.update_layout(width=670)
-    st.plotly_chart(rdfor)
     frdfor.update_layout(width=670)
     st.plotly_chart(frdfor)
-
-#KNN
-dist2 = st.expander('KNN')
-
-with dist2:
-    knn.update_layout(width=670)
-    st.plotly_chart(knn)
 
 #CatBoost
 dist3 = st.expander('CatBoost')
 
 with dist3:
-    catboost.update_layout(width=670)
-    st.plotly_chart(catboost)
     fcb.update_layout(width=670)
     st.plotly_chart(fcb)
-
-#Rede Neural
-dist4 = st.expander('Rede Neural')
-
-with dist4:
-    rn.update_layout(width=670)
-    st.plotly_chart(rn)
 
 #Regressão Logística
 dist5 = st.expander('Regressão Logística')
 
 with dist5:
-    reglog.update_layout(width=670)
-    st.plotly_chart(reglog)
     freglog.update_layout(width=670)
     st.plotly_chart(freglog)
